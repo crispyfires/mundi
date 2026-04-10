ba31f4aa3249fb0627f92f323ea1b729c39d40ce
Ignacio Casal Quinteiro <qignacio@amazon.com>
2026-04-10 15:37:05 +0200

    Revert "Add system-data-dir meson option for sound theme path"
    This reverts commit 1a7c5c36eb080e51fe1862ed0c18963bf5a88ed9.

--- build.rs
+++ build.rs
@@ -18,7 +18,6 @@ fn generate_config() {
                 "APPLICATION_ID" => "io.github.nacho.mundi".to_string(),
                 "GETTEXT_PACKAGE" => "mundi".to_string(),
                 "DATADIR" => "/usr/share".to_string(),
-                "SYSTEM_DATADIR" => "/usr/share".to_string(),
                 _ => String::new(),
             })
         })
