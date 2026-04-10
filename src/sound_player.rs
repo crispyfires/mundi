acb03cf1c758e7690b89aa7f1a411c8846a7683e
Ignacio Casal Quinteiro <qignacio@amazon.com>
2026-04-10 15:42:44 +0200

    Use MediaFile::for_resource to load sounds
    Fixes crash with GStreamer backend which doesn't support
    MediaFile::for_input_stream.

--- src/sound_player.rs
+++ src/sound_player.rs
@@ -6,19 +6,12 @@ pub struct SoundPlayer {
     settings: gio::Settings,
 }
 
-fn media_file_for_resource(resource_path: &str) -> gtk::MediaFile {
-    let bytes = gio::resources_lookup_data(resource_path, gio::ResourceLookupFlags::NONE)
-        .expect("sound resource not found");
-    let stream = gio::MemoryInputStream::from_bytes(&bytes);
-    gtk::MediaFile::for_input_stream(&stream)
-}
-
 impl Default for SoundPlayer {
     fn default() -> Self {
         let prefix = "/io/github/nacho/mundi/sounds";
         Self {
-            correct: media_file_for_resource(&format!("{prefix}/correct.oga")),
-            wrong: media_file_for_resource(&format!("{prefix}/wrong.oga")),
+            correct: gtk::MediaFile::for_resource(&format!("{prefix}/correct.oga")),
+            wrong: gtk::MediaFile::for_resource(&format!("{prefix}/wrong.oga")),
             settings: gio::Settings::new("io.github.nacho.mundi"),
         }
     }
